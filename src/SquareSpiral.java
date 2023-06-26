9e2581150a690ba7c0493631bb05924b528bb06f
650615029-Yotsawin <650615029-Yotsawin@bitbucket.org>
2023-06-26 09:20:00 +0700

    Initial

---
+++ src/SquareSpiral.java
@@ -0,0 +1,23 @@
+public class SquareSpiral
+
+{
+
+    public static void main(String[] args) 
+
+    {
+
+        Turtle bob = new Turtle();
+
+        for(int i=0;i<900;i++)
+
+        {
+
+            bob.forward(i*1.25);
+
+            bob.left(90.25);
+
+        }
+
+    }
+
+}
